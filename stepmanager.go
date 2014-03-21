67643850846a6d3a46278a3bf0ac10806cd21e22
Raphael Meyer <raphael.meyer@bbv.ch>
2014-03-21 18:17:41 +0100

    Add map to keep steps.

--- stepmanager.go
+++ stepmanager.go
@@ -6,15 +6,27 @@ type IStepManager interface {
 }
 
 type StepManager struct {
+	steps map[int]string
+	id    int
 }
 
 func (steps *StepManager) MatchingStep(step string) (bool, int) {
-	if step == "pattern" {
-		return true, 1
+	for id, pattern := range steps.steps {
+		if step == pattern {
+			return true, id
+		}
 	}
 	return false, 0
 }
 
 func (steps *StepManager) AddStep(pattern string) {
+	if steps.steps == nil {
+		steps.steps = make(map[int]string)
+	}
+	steps.steps[steps.nextId()] = pattern
+}
 
+func (steps *StepManager) nextId() int {
+	steps.id++
+	return steps.id
 }
--- stepmanager_test.go
+++ stepmanager_test.go
@@ -22,3 +22,20 @@ func Test_matching_a_step_returns_a_positive_id(t *testing.T) {
 	assert.True(t, found)
 	assert.True(t, 0 < id)
 }
+
+func Test_two_different_steps_return_a_different_id(t *testing.T) {
+	testee := &StepManager{}
+
+	first_pattern := "a pattern"
+	second_pattern := "another pattern"
+	testee.AddStep(first_pattern)
+	testee.AddStep(second_pattern)
+
+	found, first_id := testee.MatchingStep(first_pattern)
+	assert.True(t, found)
+
+	found, second_id := testee.MatchingStep(second_pattern)
+	assert.True(t, found)
+
+	assert.NotEqual(t, first_id, second_id)
+}
