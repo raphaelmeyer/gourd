7e6ba8f00893e6de8e911192bf84e5bf3642ce3d
raphael meyer <raphael.akira.meyer@gmail.com>
2015-04-05 13:47:37 +0200

    Reformat test input

--- argument_test.go
+++ argument_test.go
@@ -22,24 +22,21 @@ func Test_panics_when_accessed_index_is_out_range(t *testing.T) {
 }
 
 func Test_access_string_arguments(t *testing.T) {
-	input := []string{"some string", "another one"}
-	testee := &gourd_arguments{input}
+	testee := &gourd_arguments{[]string{"some string", "another one"}}
 
 	assert.Equal(t, "some string", testee.String(0))
 	assert.Equal(t, "another one", testee.String(1))
 }
 
 func Test_access_integer_arguemnts(t *testing.T) {
-	input := []string{"123", "-45"}
-	testee := &gourd_arguments{input}
+	testee := &gourd_arguments{[]string{"123", "-45"}}
 
 	assert.Equal(t, 123, testee.Int(0))
 	assert.Equal(t, -45, testee.Int(1))
 }
 
 func Test_arguments_can_be_accessed_multiple_times(t *testing.T) {
-	input := []string{"some string", "-71"}
-	testee := &gourd_arguments{input}
+	testee := &gourd_arguments{[]string{"some string", "-71"}}
 
 	assert.Equal(t, -71, testee.Int(1))
 	assert.Equal(t, -71, testee.Int(1))
@@ -56,8 +53,7 @@ func Test_access_order_does_not_matter(t *testing.T) {
 func Test_panics_if_argument_cannot_be_converted_into_an_integer(t *testing.T) {
 	testee := &gourd_arguments{[]string{"not a number"}}
 
-	assert.Panics(t, func(){
+	assert.Panics(t, func() {
 		testee.Int(0)
 	})
 }
-
